9726f8275998b9d53e6e7ad6db5353308b97a383
Matthew Polsom <matthewpolsom@hotmail.com>
2019-02-10 15:38:16 -0600

    Finished

--- src/app/username/username.component.ts
+++ src/app/username/username.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-username',
-  templateUrl: './username.component.html',
-  styleUrls: ['./username.component.css']
+    selector: 'app-username',
+    templateUrl: './username.component.html',
+    styleUrls: ['./username.component.css']
 })
 export class UsernameComponent implements OnInit {
+    disableResetUsername = true;
+    username = '';
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    checkInput(event: Event) {
+        if ((event.target as HTMLInputElement).value === '') {
+            this.disableResetUsername = true;
+        } else {
+            this.disableResetUsername = false;
+        }
+    }
+
+    resetUsername() {
+        this.username = '';
+        this.disableResetUsername = true;
+    }
 }

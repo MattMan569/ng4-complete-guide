a8d87a1a99c61c5b2b52c031467207f6cca202b0
Matthew Polsom <matthewpolsom@hotmail.com>
2019-02-14 12:22:10 -0600

    Formatting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,12 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { ActiveUsersComponent } from './active-users/active-users.component';
 import { InactiveUsersComponent } from './inactive-users/inactive-users.component';
+import { UsersService } from './users.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ActiveUsersComponent,
-    InactiveUsersComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, ActiveUsersComponent, InactiveUsersComponent],
+    imports: [BrowserModule, FormsModule, HttpModule],
+    providers: [UsersService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}

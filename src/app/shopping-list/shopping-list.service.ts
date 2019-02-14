da24a220b58eaa9f450b1281ae390036d5cda2ea
Matthew Polsom <matthewpolsom@hotmail.com>
2019-02-14 15:10:15 -0600

    Lecture 111 - Added event on ingredients changed

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,6 +1,10 @@
 import { Ingredient } from '../shared/ingredient.model';
+import { EventEmitter } from '@angular/core';
 
 export class ShoppingListService {
+    // Notify listeners that the ingredient list has changed
+    ingredientsChanged = new EventEmitter<Ingredient[]>();
+
     private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
         new Ingredient('Tomatoes', 10)
@@ -14,5 +18,8 @@ export class ShoppingListService {
 
     addIngredient(ingredient: Ingredient) {
         this.ingredients.push(ingredient);
+
+        // Broadcast a new copy of the changed array
+        this.ingredientsChanged.emit(this.ingredients.slice());
     }
 }

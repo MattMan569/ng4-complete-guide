630a10dd807a03fc426e25dcb29b5a8d06184019
Matthew Polsom <matthewpolsom@hotmail.com>
2019-02-14 15:17:46 -0600

    Lecture 112 - Displaying recipe ingredients

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,7 @@
 import { EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
@@ -9,12 +10,14 @@ export class RecipeService {
         new Recipe(
             'A Test Recipe',
             'This is simply a test',
-            'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+            'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+            [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
         ),
         new Recipe(
             'Another Test Recipe',
             'This is simply another test',
-            'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+            'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+            [new Ingredient('Buns', 1), new Ingredient('Meat', 1)]
         )
     ];
 

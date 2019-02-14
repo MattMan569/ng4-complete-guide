b24992ff90ed56e531d3202e7ab45445279d89c8
Matthew Polsom <matthewpolsom@hotmail.com>
2019-02-14 14:43:23 -0600

    Lecture 109 - Moved recipes to service

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,26 @@
+import { Recipe } from './recipe.model';
+
+export class RecipeService {
+    private recipes: Recipe[] = [
+        new Recipe(
+            'A Test Recipe',
+            'This is simply a test',
+            'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+        ),
+        new Recipe(
+            'Another Test Recipe',
+            'This is simply another test',
+            'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+        )
+    ];
+
+    constructor() {}
+
+    getRecipes() {
+        // Return a direct reference to the array
+        // return this.recipes;
+
+        // Return a new copy of the array
+        return this.recipes.slice();
+    }
+}

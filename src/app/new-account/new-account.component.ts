eafc06b6e6691420f4f937cbcfb4c9b790eca996
Matthew Polsom <matthewpolsom@hotmail.com>
2019-02-13 22:47:29 -0600

    Lecture 99

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,18 +1,28 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { LoggingService } from '../logging.service';
 
 @Component({
     selector: 'app-new-account',
     templateUrl: './new-account.component.html',
-    styleUrls: ['./new-account.component.css']
+    styleUrls: ['./new-account.component.css'],
+    providers: [LoggingService] // Specify the services this component uses
 })
 export class NewAccountComponent {
     @Output() accountAdded = new EventEmitter<{ name: string; status: string }>();
+
+    // Service injection
+    // Specify the service type in the constructor,
+    // do not instantiate the service anywhere else
+    constructor(private loggingService: LoggingService) {}
 
     onCreateAccount(accountName: string, accountStatus: string) {
         this.accountAdded.emit({
             name: accountName,
             status: accountStatus
         });
-        console.log('A server status changed, new status: ' + accountStatus);
+        // console.log('A server status changed, new status: ' + accountStatus);
+
+        // Use the service
+        this.loggingService.logStatusChange(accountStatus);
     }
 }

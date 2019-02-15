a3affef7da78e72a75d4c1afa574ce442dc1cffc
Matthew Polsom <matthewpolsom@hotmail.com>
2019-02-14 19:04:25 -0600

    Lecture 121 - Programmatic routing

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-home',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-    constructor() {}
+    constructor(private router: Router) {}
 
     ngOnInit() {}
+
+    // Navigate to the servers component programmatically
+    // rather than with <a routerLink="/servers">Servers</a>
+    onLoadServers() {
+        this.router.navigate(['/servers']);
+    }
 }

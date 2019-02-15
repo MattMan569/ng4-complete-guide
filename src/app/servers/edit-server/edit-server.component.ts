0bd88a23bdf16e8f336dcabd6930f008a05029c4
Matthew Polsom <matthewpolsom@hotmail.com>
2019-02-15 17:46:45 -0600

    Lecture 128 - Retrieving queries and fragments

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -12,9 +13,20 @@ export class EditServerComponent implements OnInit {
     serverName = '';
     serverStatus = '';
 
-    constructor(private serversService: ServersService) {}
+    constructor(private serversService: ServersService, private route: ActivatedRoute) {}
 
     ngOnInit() {
+        // Retrieve the query parameters and the fragment
+
+        // Wont work if the component does not change
+        // The component is not re-rendered with the new info
+        console.log(this.route.snapshot.queryParams);
+        console.log(this.route.snapshot.fragment);
+
+        // Subscribe to watch for any changes
+        this.route.queryParams.subscribe();
+        this.route.fragment.subscribe();
+
         this.server = this.serversService.getServer(1);
         this.serverName = this.server.name;
         this.serverStatus = this.server.status;

6828476b94d6ca5a2a7c184b51ebbba51dbb3dad
Matthew Polsom <matthewpolsom@hotmail.com>
2019-02-16 15:36:26 -0600

    Lecture 155 - Add editing routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full' },
@@ -12,8 +13,11 @@ const appRoutes: Routes = [
         path: 'recipes',
         component: RecipesComponent,
         children: [
+            // Paths with dynamic params should always go below static links
             { path: '', component: RecipeStartComponent },
-            { path: ':id', component: RecipeDetailComponent }
+            { path: 'new', component: RecipeEditComponent },
+            { path: ':id', component: RecipeDetailComponent },
+            { path: ':id/edit', component: RecipeEditComponent }
         ]
     },
     { path: 'shopping-list', component: ShoppingListComponent }

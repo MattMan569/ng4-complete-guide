ebfb32b60e3812c02f13c1356250f9976590c176
Matthew Polsom <matthewpolsom@hotmail.com>
2019-02-16 15:42:35 -0600

    Lecture 156 - Retrieving query params in edit recipe

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
-  selector: 'app-recipe-edit',
-  templateUrl: './recipe-edit.component.html',
-  styleUrls: ['./recipe-edit.component.css']
+    selector: 'app-recipe-edit',
+    templateUrl: './recipe-edit.component.html',
+    styleUrls: ['./recipe-edit.component.css']
 })
 export class RecipeEditComponent implements OnInit {
+    id: number;
+    editMode = false;
 
-  constructor() { }
+    constructor(private route: ActivatedRoute) {}
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.route.params.subscribe((params: Params) => {
+            this.id = +params.id;
 
+            // Edit mode implied that the id of a recipe to
+            // edit has also been passed as a query param
+            this.editMode = params.id != null;
+        });
+    }
 }

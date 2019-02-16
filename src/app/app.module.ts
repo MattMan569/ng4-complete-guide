8d4c8e66ead7acd481ad3cbdf3b5fd5620246eb4
Matthew Polsom <matthewpolsom@hotmail.com>
2019-02-15 22:28:38 -0600

    Lecture 133 - Handling invalid routes, redirection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 // Hold all of the app's routes
 const appRoutes: Routes = [
@@ -38,7 +39,13 @@ const appRoutes: Routes = [
             { path: ':id', component: ServerComponent },
             { path: ':id/edit', component: EditServerComponent }
         ]
-    }
+    },
+
+    // Page not found / invalid route
+    { path: 'not-found', component: PageNotFoundComponent },
+
+    // Wildcard route, this should be the last route
+    { path: '**', redirectTo: '/not-found' }
 ];
 
 @NgModule({
@@ -49,7 +56,8 @@ const appRoutes: Routes = [
         ServersComponent,
         UserComponent,
         EditServerComponent,
-        ServerComponent
+        ServerComponent,
+        PageNotFoundComponent
     ],
     // Import RouterModule and register the routes
     imports: [BrowserModule, FormsModule, HttpModule, RouterModule.forRoot(appRoutes)],

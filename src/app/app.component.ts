7171c71cb3630447ec36096ccc1ad36b74c62fd5
Matthew Polsom <matthewpolsom@hotmail.com>
2019-02-17 17:18:35 -0600

    Progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, DoCheck } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements DoCheck {
+    @ViewChild('form') form: NgForm;
 
+    subscriptionTypes = ['Basic', 'Advanced', 'Pro'];
+    defaultSubscription = this.subscriptionTypes[1];
+    allTouched = false;
+    submittedForm = {};
+
+    ngDoCheck() {
+        // Check all inputs except those with default values
+        let allInputsTouched = true;
+        (<any>Object).values(this.form.controls).forEach(control => {
+            if (!control.touched && control !== this.form.controls['subscription']) {
+                allInputsTouched = false;
+            }
+        });
+        this.allTouched = allInputsTouched;
+    }
+
+    onSubmit() {
+        this.submittedForm = this.form.value;
+        console.log(this.submittedForm);
+    }
 }

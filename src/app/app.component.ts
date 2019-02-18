4cb161082061d38db2a0580b757d78d2005ad842
Matthew Polsom <matthewpolsom@hotmail.com>
2019-02-18 11:38:43 -0600

    Lecture 200 - Custom validator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 })
 export class AppComponent implements OnInit {
     genders = ['male', 'female'];
+    forbiddenUsernames = ['Chris', 'Anna'];
 
     signupForm: FormGroup;
 
@@ -19,7 +20,13 @@ export class AppComponent implements OnInit {
 
             // Can create another form group to group form controls together
             userData: new FormGroup({
-                username: new FormControl(null, Validators.required),
+                username: new FormControl(null, [
+                    Validators.required,
+                    // Will be called by Angular, outside of the class
+                    // Must bind it to this class to ensure 'this' in our function
+                    // refers to properties of our class
+                    this.forbiddenNames.bind(this)
+                ]),
                 email: new FormControl(null, [Validators.required, Validators.email])
             }),
 
@@ -53,5 +60,21 @@ export class AppComponent implements OnInit {
     // Explicity define the type to prevent an error message in the HTML code
     getHobbies() {
         return (<FormArray>this.signupForm.get('hobbies')).controls;
+    }
+
+    // Create a custom validator
+    // Returns a key value pair (key: string, value: bool)
+    forbiddenNames(control: FormControl): { [key: string]: boolean } {
+        // If the username is a key of the forbidden usernames
+        // Returns -1 if it is not found, which is interpretted as true by JS
+        if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+            return { nameIsForbidden: true };
+        }
+        // If validation is successful, you must pass
+        // nothing or null, not false.
+        return null;
+
+        // Wrong:
+        // return { nameIsForbidden: false };
     }
 }

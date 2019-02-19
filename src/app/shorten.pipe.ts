526471a5388bc9578bff98cb36209ebefe817747
Matthew Polsom <matthewpolsom@hotmail.com>
2019-02-19 17:24:13 -0600

    Lecture 233 - Custom pipe parameters

--- src/app/shorten.pipe.ts
+++ src/app/shorten.pipe.ts
@@ -1,12 +1,23 @@
+// To create a custom pipe:
+//
+// 1) Add pipe decorator
+// 2) Implement the transform method
+// 3) Add the pipe to the module's declarations array
+
 import { PipeTransform, Pipe } from '@angular/core';
 
 @Pipe({
     name: 'shorten'
 })
 export class ShortenPipe implements PipeTransform {
-    transform(value: any) {
-        if (value.length > 10) {
-            return value.substr(0, 10) + ' ...';
+    // Args: pipe input data, pipe parameters
+    transform(value: any, limit: number, trail?: string) {
+        if (!trail) {
+            trail = ' ...';
+        }
+
+        if (value.length > limit) {
+            return value.substr(0, limit) + trail;
         } else {
             return value;
         }

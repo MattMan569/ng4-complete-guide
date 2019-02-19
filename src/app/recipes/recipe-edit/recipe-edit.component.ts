638ca03e691d2dba70147c202f2cc21f97ddf2b1
Matthew Polsom <matthewpolsom@hotmail.com>
2019-02-19 14:11:36 -0600

    Lecture 219 - Added add ingredient button

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -35,6 +35,17 @@ export class RecipeEditComponent implements OnInit {
         console.log(this.recipeForm);
     }
 
+    // Add a new control group the the ingredient
+    // list's array of form control groups
+    onAddIngredient() {
+        (this.recipeForm.get('ingredients') as FormArray).push(
+            new FormGroup({
+                name: new FormControl(),
+                amount: new FormControl()
+            })
+        );
+    }
+
     private initForm() {
         let recipeName = '';
         let recipeImagePath = '';

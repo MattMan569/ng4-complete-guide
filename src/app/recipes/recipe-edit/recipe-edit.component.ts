d1be19dc624377af51cbd474d9444c686a842db4
Matthew Polsom <matthewpolsom@hotmail.com>
2019-02-19 13:58:46 -0600

    Lecture 217 - Adding ingredients to the edit recipe form

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
 
 import { RecipeService } from '../recipe.service';
 
@@ -39,18 +39,36 @@ export class RecipeEditComponent implements OnInit {
         let recipeName = '';
         let recipeImagePath = '';
         let recipeDescription = '';
+        const recipeIngredients = new FormArray([]);
 
         // Populate the values if a recipe is being edited
         if (this.editMode) {
-            recipeName = this.recipeService.getRecipe(this.id).name;
-            recipeImagePath = this.recipeService.getRecipe(this.id).imagePath;
-            recipeDescription = this.recipeService.getRecipe(this.id).description;
+            const selectedRecipe = this.recipeService.getRecipe(this.id);
+            recipeName = selectedRecipe.name;
+            recipeImagePath = selectedRecipe.imagePath;
+            recipeDescription = selectedRecipe.description;
+
+            // If the recipe has ingredients
+            if (selectedRecipe.ingredients) {
+                // Push each ingredient to the list
+                for (const ingredient of selectedRecipe.ingredients) {
+                    recipeIngredients.push(
+                        // Each recipe is a form group with two controls,
+                        // its name and amount
+                        new FormGroup({
+                            name: new FormControl(ingredient.name),
+                            amount: new FormControl(ingredient.amount)
+                        })
+                    );
+                }
+            }
         }
 
         this.recipeForm = new FormGroup({
             name: new FormControl(recipeName),
             imagePath: new FormControl(recipeImagePath),
-            description: new FormControl(recipeDescription)
+            description: new FormControl(recipeDescription),
+            ingredients: recipeIngredients
         });
     }
 }

d30d3e10c73ec2faecc6d4ca294ee3d49c8dbb45
Matthew Polsom <matthewpolsom@hotmail.com>
2019-02-20 00:25:02 -0600

    Lecture 255 - Validating response data to prevent errors

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 
+import { map } from 'rxjs/operators';
+
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
 
@@ -18,8 +20,24 @@ export class DataStorageService {
     getRecipes() {
         this.http
             .get('https://ng-recipe-book-polsom2m.firebaseio.com/recipes.json')
-            .subscribe((response: Response) => {
-                const recipes: Recipe[] = response.json();
+            .pipe(
+                map((response: Response) => {
+                    const recipes: Recipe[] = response.json();
+
+                    // If a recipe is pushed to the database with no ingredients,
+                    // it will node contain an ingredient node on the database.
+                    // Thus, there will be no ingredient field on that fetched recipe.
+                    // Here we ensure each recipe has an ingredient array.
+                    for (const recipe of recipes) {
+                        if (!recipe.ingredients) {
+                            recipe.ingredients = [];
+                        }
+                    }
+
+                    return recipes;
+                })
+            )
+            .subscribe((recipes: Recipe[]) => {
                 this.recipeService.setRecipes(recipes);
             });
     }

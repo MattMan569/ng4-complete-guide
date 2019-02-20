a5d97339b8846f688c312b7556a83ed3706c2857
Matthew Polsom <matthewpolsom@hotmail.com>
2019-02-20 11:22:59 -0600

    Lecture 260 - Set up the firebase SDK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     loadedFeature = 'recipe';
+
+    ngOnInit() {
+        // Get info from authentication -> 'Web Setup'
+        firebase.initializeApp({
+            apiKey: 'AIzaSyB8MwYtAlxz7Hx00DpNe3hMbrAXypWAiDU',
+            authDomain: 'ng-recipe-book-polsom2m.firebaseapp.com'
+        });
+    }
 
     onNavigate(feature: string) {
         this.loadedFeature = feature;

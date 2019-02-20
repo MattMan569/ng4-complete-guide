025f062cfe5f9cd6c1e17e4d708ede79984264a8
Matthew Polsom <matthewpolsom@hotmail.com>
2019-02-19 21:13:02 -0600

    Lecture 242 - Custom headers

--- src/app/server.service.ts
+++ src/app/server.service.ts
@@ -7,9 +7,17 @@ export class ServerService {
     constructor(private http: Http) {}
 
     storeServers(servers: any[]) {
+        // We don't need custom headers,
+        // but some backends might
+        const headers = new Headers({
+            'Content-Type': 'application/json'
+        });
+
         // The post method only creates an observable, not an actual post request.
         // It must be subscibed to before it will be sent.
         // With firebase, an endpoint can be specified (data.json)
-        return this.http.post('https://udemy-ng-http-polsom2m.firebaseio.com/data.json', servers);
+        return this.http.post('https://udemy-ng-http-polsom2m.firebaseio.com/data.json', servers, {
+            headers: headers
+        });
     }
 }

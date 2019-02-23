b0e0be20cdaab138cc8da2be090b4fbbb6049aba
Matthew Polsom <matthewpolsom@hotmail.com>
2019-02-23 11:50:21 -0600

    Lecture 277 - Set up recipes feature routes

---
+++ src/app/recipes/recipes-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { RecipesComponent } from './recipes.component';
+import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+
+import { AuthGuard } from '../auth/auth-guard.service';
+
+const recipesRoutes: Routes = [
+    {
+        path: 'recipes',
+        component: RecipesComponent,
+        children: [
+            // Paths with dynamic params should always go below static links of the same path
+            { path: '', component: RecipeStartComponent },
+            { path: 'new', component: RecipeEditComponent, canActivate: [AuthGuard] },
+            { path: ':id', component: RecipeDetailComponent },
+            { path: ':id/edit', component: RecipeEditComponent, canActivate: [AuthGuard] }
+        ]
+    }
+];
+
+@NgModule({
+    // Use forChild instead of forRoot, forRoot is only for the appModule
+    imports: [RouterModule.forChild(recipesRoutes)],
+    exports: [RouterModule]
+})
+export class RecipesRoutingModule {}
--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { RecipesRoutingModule } from './recipes-routing.module';
+
 import { RecipesComponent } from './recipes.component';
 import { RecipeStartComponent } from './recipe-start/recipe-start.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
-
-import { DropdownDirective } from '../shared/dropdown.directive';
-import { AppRoutingModule } from '../app-routing.module';
 
 @NgModule({
     // Declarations must not appear in more than one module
@@ -25,6 +24,6 @@ import { AppRoutingModule } from '../app-routing.module';
 
     // Very important: always import the common module
     // in every feature module
-    imports: [CommonModule, ReactiveFormsModule, AppRoutingModule]
+    imports: [CommonModule, ReactiveFormsModule, RecipesRoutingModule]
 })
 export class RecipesModule {}
